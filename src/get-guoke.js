bcf69eb6a07246d81466c0bc79d43f929fbcd5b5
rahul <rahul.wu@vipshop.com>
2016-03-04 23:32:04 +0800

    [UPD]更新项目代码

--- src/get-guoke.js
+++ src/get-guoke.js
@@ -14,6 +14,7 @@ var AV = require('leanengine');
 var request = require('superagent');
 var colors = require('colors');
 var base = 'http://www.guokr.com/apis/minisite/article.json';
+var cheerio = require('cheerio');
 var config = {
     tagList: [{
         "id": "biology",
@@ -22,28 +23,151 @@ var config = {
 };
 
 var biologylist = AV.Object.extend('biologylist');  //生物文章列表对象
+var _stream = fs.createWriteStream('../log/biology.js', {
+    flags: 'a',
+    encoding: 'utf-8'
+})
 
-//console.log(123);
-getBiogyList();
-function getBiogyList() {
+getBiogyList(0,100);
+
+/**
+ * 爬去数据
+ * @param offset
+ * @param size
+ */
+function getBiogyList(offset,size) {
     var tag = config.tagList[0]
     var tid = tag.id;
     var tname = tag.name
-   //url http://www.guokr.com/apis/minisite/article.json?retrieve_type=by_subject&subject_key=biology&limit=20&offset=0&_=1457054116268
     var params = {
         retrieve_type:'by_subject',
         subject_key:tid,
-        limit:100,
-        offset:0,
+        limit:size,
+        offset:offset,
         _:new Date().getTime()
     }
+   // getPageDelay()
     request.get(base)
         .query(params)
         .end(function(err, res){
-            //console.log(res.text);
-            console.log(res.body);
+            if(err){
+                _log(err);
+            }
+            var resultArr = res.body.result
+            for(var i = 0;i < resultArr.length;i++){
+                var detailUrl = resultArr[i].url;
+                console.log(detailUrl);
+            }
         })
 }
+
+
+//function getPageDelay(params, callback){
+//    //var url = base + page
+//
+//    getListFromUrl(params, function (data){
+//        data['tag'] = ctag
+//        postArticle(data)	// 写入avos
+//    },function(){
+//        page += 25
+//        console.log(index, page)
+//        if(page > 925){
+//            callback && callback()
+//            return
+//        }
+//        setTimeout(function(){
+//            getPageDelay(base, page, ctag, callback)
+//        }, 3000)
+//    })
+//}
+
+/**
+ * 编写日志
+ * @param content  日志内容
+ * @private
+ */
+function _log(content){
+    _stream.write(content)
+}
+
+function getListFromUrl(url, cbData, cbDone){
+    console.log('get url:', url)
+    osmosis
+        .config({
+            tries: 1,
+            timeout: 10 * 60 * 1000,
+            concurrency: 1
+        })
+        .get(url)	// 列表页url
+        .find('.pagedlist_item')
+        .set({
+            'account_avatar': '.profile_photo_img@src',		// 微信公众账号avatar
+            'update_time': 'span.timestamp',
+            'article_id': '.question_link@href',			// 文章id 对应的是 chuansong.me 上的id
+        })
+        .data(function (listing){
+            _num++
+            cbData && cbData(listing)	// 返回数据
+
+            console.log(colors.yellow.underline(_num), colors.yellow(listing.article_id))
+            _log(fmt(_num + listing.article_id + '\n'))
+        })
+        .done(function(s){
+            cbDone && cbDone()	// 一次爬虫完毕
+        })
+        .error(function(err){
+            _log(fmt(err))
+        })
+}
+
+/**
+ * fing article by id in leancloud
+ */
+//function findArticleById(id, callback){
+//    var queryAt = new AV.Query('Article')
+//
+//    queryAt.equalTo("article_id", id)
+//
+//    queryAt.find({
+//        success: function(results) {
+//            if(results.length > 0){
+//                console.log('存在', id, results.length)
+//                callback && callback(true)
+//            }else{
+//                callback && callback(false)
+//            }
+//        },
+//        error: function(error) {
+//            callback && callback(false)
+//        }
+//    })
+//}
+
+/**
+ * 延迟推送
+ * @param base
+ * @param page
+ * @param ctag
+ * @param callback
+ */
+//function getPageDelay(base, page, ctag, callback){
+//    var url = base + page
+//
+//    getListFromUrl(url, function (data){
+//        data['tag'] = ctag
+//        postArticle(data)	// 写入avos
+//    },function(){
+//        page += 25
+//        console.log(index, page)
+//        if(page > 925){
+//            callback && callback()
+//            return
+//        }
+//        setTimeout(function(){
+//            getPageDelay(base, page, ctag, callback)
+//        }, 3000)
+//    })
+//}
 
 //function getPageFormTag(){
 //    var tag = config.tagList[0]
