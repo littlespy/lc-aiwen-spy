bbe58f4eeb203d03a23ee4ba693622f3e0710723
rahul <rahul.wu@vipshop.com>
2016-03-05 17:17:16 +0800

    [UPD]更新爬虫

--- src/get-guoke.js
+++ src/get-guoke.js
@@ -57,14 +57,6 @@ function getBiogyList(offset,size) {
             for(var i = 0;i < resultArr.length;i++){
                 var detailUrl = resultArr[i].resource_url;
 
-                //articalData.id = resultArr[i].id;
-                //articalData.type = resultArr[i].subject.name;
-                //articalData.key = resultArr[i].subject.key;
-                //articalData.author = resultArr[i].author;
-                //articalData.listimg = resultArr[i].image_info;
-                //articalData.summary = resultArr[i].summary;
-                //articalData.title = resultArr[i].title_hide;
-                //articalData.updatetime = resultArr[i].date_modified;
                 //接着扒取详情页内容数据
                     request.get(detailUrl)
                     .end(function(err, res){
@@ -73,42 +65,24 @@ function getBiogyList(offset,size) {
                         }else{
                             var resultObj = res.body.result;
                             var articalData = {};
-                            articalData.code='200';
-                            articalData.data = {};
                             articalData.id = resultObj.id;
-                            articalData.data.content = resultObj.content;
+                            articalData.content = resultObj.content;
                             articalData.subject = resultObj.subject;
                             articalData.author = resultObj.author;
+                            articalData.tag = 'biology';
                             articalData.listimg = resultObj.image_info;
                             articalData.summary = resultObj.summary;
                             articalData.title = resultObj.title;
                             articalData.updatetime = resultObj.date_modified;
-                            console.log(JSON.stringify(articalData));
+                            //console.log(JSON.stringify(articalData));
+                            postArticle(articalData,function(){
+
+                            })
                         }
                     });
             }
         });
 }
-
-
-//function getPageDelay(params, callback){
-//    //var url = base + page
-//
-//    getListFromUrl(params, function (data){
-//        data['tag'] = ctag
-//        postArticle(data)	// 写入avos
-//    },function(){
-//        page += 25
-//        console.log(index, page)
-//        if(page > 925){
-//            callback && callback()
-//            return
-//        }
-//        setTimeout(function(){
-//            getPageDelay(base, page, ctag, callback)
-//        }, 3000)
-//    })
-//}
 
 /**
  * 编写日志
@@ -119,96 +93,73 @@ function _log(content){
     _stream.write(content)
 }
 
-function getListFromUrl(url, cbData, cbDone){
-    console.log('get url:', url)
-    osmosis
-        .config({
-            tries: 1,
-            timeout: 10 * 60 * 1000,
-            concurrency: 1
-        })
-        .get(url)	// 列表页url
-        .find('.pagedlist_item')
-        .set({
-            'account_avatar': '.profile_photo_img@src',		// 微信公众账号avatar
-            'update_time': 'span.timestamp',
-            'article_id': '.question_link@href',			// 文章id 对应的是 chuansong.me 上的id
-        })
-        .data(function (listing){
-            _num++
-            cbData && cbData(listing)	// 返回数据
 
-            console.log(colors.yellow.underline(_num), colors.yellow(listing.article_id))
-            _log(fmt(_num + listing.article_id + '\n'))
+/**
+ * post article 到 avoscloud
+ */
+function postArticle (data, callback){
+    var bio = new biologylist()
+
+    if(!data.id) return
+
+    var bioid = data.id
+
+    findArticleById(bioid, function(flag){
+        if(flag) return;
+        // 设置数据
+
+        //articalData.id = resultObj.id;
+        //articalData.content = resultObj.content;
+        //articalData.subject = resultObj.subject;
+        //articalData.author = resultObj.author;
+        //articalData.tag = 'biology';
+        //articalData.listimg = resultObj.image_info;
+        //articalData.summary = resultObj.summary;
+        //articalData.title = resultObj.title;
+        //articalData.updatetime = resultObj.date_modified;
+
+        bio.set('id', data.id)
+        bio.set('update_time', params.update_time)
+        bio.set('article_id', ar_id)
+        bio.set('tag_id', params.tag.id)
+        bio.set('tag_name', params.tag.name)
+        bio.save().then(function (obj) {
+            //对象保存成功
+            console.log('保存成功')
+            // console.log(obj)
+        }, function (error) {
+            //对象保存失败，处理 error
+            _log(error)
+        }).always(function(){
+            //无论成功还是失败，都调用到这里
+            callback && callback('done')
         })
-        .done(function(s){
-            cbDone && cbDone()	// 一次爬虫完毕
-        })
-        .error(function(err){
-            _log(fmt(err))
-        })
+    })
+
 }
 
-/**
- * fing article by id in leancloud
- */
-//function findArticleById(id, callback){
-//    var queryAt = new AV.Query('Article')
-//
-//    queryAt.equalTo("article_id", id)
-//
-//    queryAt.find({
-//        success: function(results) {
-//            if(results.length > 0){
-//                console.log('存在', id, results.length)
-//                callback && callback(true)
-//            }else{
-//                callback && callback(false)
-//            }
-//        },
-//        error: function(error) {
-//            callback && callback(false)
-//        }
-//    })
-//}
 
 /**
- * 延迟推送
- * @param base
- * @param page
- * @param ctag
+ * 根据id查询文章
+ * @param id
  * @param callback
  */
-//function getPageDelay(base, page, ctag, callback){
-//    var url = base + page
-//
-//    getListFromUrl(url, function (data){
-//        data['tag'] = ctag
-//        postArticle(data)	// 写入avos
-//    },function(){
-//        page += 25
-//        console.log(index, page)
-//        if(page > 925){
-//            callback && callback()
-//            return
-//        }
-//        setTimeout(function(){
-//            getPageDelay(base, page, ctag, callback)
-//        }, 3000)
-//    })
-//}
+function findArticleById(id, callback){
+    var queryAt = new AV.Query('biologylist')
 
-//function getPageFormTag(){
-//    var tag = config.tagList[0]
-//    var tid = tag.id;
-//    var tname = tag.name
-//    var base = 'http://www.guokr.com/scientific/subject/'
-//
-//    var url = base + tid + '/'
-//
-//    getPageDelay(url, 0, tag, function(){
-//        index ++
-//        if(index > config.tagList.length -1) return
-//        getPageFormTag()
-//    })
-//}
+    queryAt.equalTo("id", id)
+
+    queryAt.find({
+        success: function(results) {
+            if(results.length > 0){
+                console.log('存在', id, results.length)
+                callback && callback(true)
+            }else{
+                callback && callback(false)
+            }
+        },
+        error: function(error) {
+            callback && callback(false)
+        }
+    })
+}
