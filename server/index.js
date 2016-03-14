8381d7239d1f556d999f49bd8278b02e8589c9b2
rahul <rahul@vipshop.com>
2016-03-14 10:06:02 +0800

    【UPD】更新服务端入口脚本
    更新服务端入口脚本代码

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ var app = express();
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var request = require('request');
+var AV = require('leanengine');
 var meta = require('../package.json');
+var init = require('../conf/init'); //工程初始化配置
 
 process.on('uncaughtException', function(err) {
 	(app.get('logger') || console).error('Uncaught exception:\n', err.stack);
@@ -21,6 +23,31 @@ app.enable('trust proxy');
 
 app.get('/', function(req, res) {
 	res.send('hello world');
+});
+
+app.get('/api/getbiolist', function(req, res) {
+	var query = new AV.Query('biologylist'); //生物列表数据
+	var response = {};
+	query = query.addDescending('bio_id');
+	query = query.limit(200);
+	query.find({
+		success: function(results) { //查询数据回调成功
+			// results is an array of AV.Object.
+			response.code = '200';
+			response.data = {};
+			response.data.results = results;
+			console.log(results.length);
+			response.msg = 'success';
+			res.send(response);
+		},
+
+		error: function(error) { //查询数据回调失败
+			response.code = '100';
+			response.msg = 'query error';
+			res.send(response);
+		}
+	});
+
 });
 
 if (require.main === module) {
